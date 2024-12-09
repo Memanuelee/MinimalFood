11b7487cc9b04d3a4ae264abaf7fb29200da3186
Emanuele Bazzicalupo <bazzicalupo.emanuelee@gmail.com>
2024-12-09 04:07:08 +0100

    Initial Commit

---
+++ MinimalFood/MinimalFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalFoodApp.swift
+//  MinimalFood
+//
+//  Created by Emanuele Bazzicalupo on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MinimalFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
